5f6035ed831fe0a3894756a365c952b860423a7e
Saurabhpipare01 <126401496+Saurabhpipare01@users.noreply.github.com>
2023-07-08 09:45:11 +0530

    JIRA[567]

--- API/src/test/java/api/endpoints/Routes.java
+++ API/src/test/java/api/endpoints/Routes.java
@@ -5,14 +5,14 @@ public class Routes {
 	
 	public static String Base_URL="https://petstore.swagger.io/v2";
 	
-	//User Model
+	//User Modet(create_user/get_user/update_user/delete_user)
 	
 	
 	public static String Post_Url=Base_URL+"/user";
 	public static String Get_Url=Base_URL+"/user/{username}";
 	public static String Update_Url=Base_URL+"/user/{username}";
 	public static String Delete_Url=Base_URL+"/user/{username}";
-
+    
 
 	
 	
